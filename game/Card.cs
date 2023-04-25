d9538dd15eb45ca3597d9abc5cca4d297bd86d88
Климочко Остап <ostap.klymochko@lnu.edu.ua>
2023-04-25 11:31:05 +0300

    init

---
+++ game/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game
+{
+    public enum Suits
+    {
+        Clubs = 1,
+        Hearts= 2,
+        Diamonds = 3,
+        Spades = 4
+    }
+    public enum Values
+    {
+        Six = 6,Seven = 7,Eight = 8,Nine = 9,Ten = 10,Jack = 11, Queen = 12, King = 13, Ace = 14
+    }
+    public class Card
+    {
+        public Values Value { get; init; }
+        public Suits Suit { get; init; }
+    }
+}
--- game/Main.cs
+++ game/Main.cs
@@ -6,7 +6,7 @@ namespace game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Values.Ace.GetHashCode());
         }
     }
 }
