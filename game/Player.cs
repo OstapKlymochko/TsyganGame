a45bce16989c2f9c17ba5fefa95c8b91b4a091e9
dmitru4ok <mobilokh4566@gmail.com>
2023-05-05 16:02:45 +0300

    Added player basic implementation

---
+++ game/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    public class Player
+    {
+        private static Random BelieveHandler = new Random();
+        public string Name { get; set; }
+
+        private List<Card> cards;
+
+        public Card? LastGiven  { get; private set; }
+
+        public int CardCount => cards.Count;
+
+        public Player(string name, IEnumerable<Card> crds)
+        {
+            Name = name;
+            cards = new List<Card>();
+        }
+
+        void TakeCard(Card card) => cards.Add(card);
+
+        Card RemoveCard(int index)
+        {
+            if (index > cards.Count - 1 || index < 0)
+                throw new ArgumentOutOfRangeException($"Index {index} is out of range");
+            LastGiven = cards[index];
+            cards.RemoveAt(index);
+            return LastGiven;
+        }
+
+        public bool Believe()
+        {
+            return BelieveHandler.Next(0, 100) <= 90;
+        }
+    }
+}
